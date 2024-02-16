b12d4dfa2d7a03588a446f3bcb64867f4e677557
cxsar21 <160218358+cxsar21@users.noreply.github.com>
2024-02-16 10:22:35 -0600

    music--list.js

--- music--list.js
+++ music--list.js
@@ -3,21 +3,21 @@ let allMusic = [
         name: "Content Explicit",
         artist: "Cosmic kid",
         img: "0001.jpg",
-        src: "Music1 COSMIC KID & YESAN - CONTENT EXPLICIT (Letra Lyrics).mp3"
+        src: "Music1 COSMIC KID & YESAN - CONTENT EXPLICIT (Letra Lyrics)"
     },
 
     {
         name: "De mi",
         artist: "Dromerario y Ed maverick",
         img: "0002.jpg",
-        src: "Music2 Dromedarios Mágicos - De Mí (feat. Ed Maverick) [Video Oficial].mp3"
+        src: "Music2 Dromedarios Mágicos - De Mí (feat. Ed Maverick) [Video Oficial]"
 },
 
 {
     name: "chachacha",
     artist: "Josean Log",
     img: "0003.jpg",
-    src: "Music3 Jósean Log - Chachachá (Lyric Video).mp3"
+    src: "Music3 Jósean Log - Chachachá (Lyric Video)"
 }
 
 
